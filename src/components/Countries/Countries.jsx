86de808fd7649c037444bc0684a64e1a90fb57f5
Afjal Hossain <abdullahalafjal31@gmail.com>
2026-03-15 02:45:14 +0600

    in arrays by comparing

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -8,7 +8,11 @@ const Countries = ({ countriesPromise }) => {
     const [visitedCountries, setVisitedCountries] = useState([]);
 
     const handleVisitedCountries = (country) => {
-        console.log('Handle visited countries clicked', country);
+        // console.log('Handle visited countries clicked', country);
+
+        // visitedCountries state er copy newa hocche, tarpor new country add kora hocche, and then setVisitedCountries diye update kora hocche
+        const newVisitedCountries = [...visitedCountries, country];
+        setVisitedCountries(newVisitedCountries);
     } 
 
     const countriesData = use(countriesPromise);
@@ -18,7 +22,14 @@ const Countries = ({ countriesPromise }) => {
 
         <div>
             <h1>In the Countries: {countries.length}</h1>
-            <h3>Total Country Visited:</h3>
+            <h3>Total Country Visited: {visitedCountries.length}</h3>
+            <ol>
+                {
+                    visitedCountries.map(country => <li 
+                        key={country.cca3?.cca3}>{country.name.common}</li>)
+                }
+            </ol>
+
             <div className="countries">
                 {
                     countries.map(country => <Country
