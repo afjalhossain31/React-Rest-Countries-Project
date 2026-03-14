d4ffa6f126911fd887364ee3f7de4293dd50113f
Afjal Hossain <abdullahalafjal31@gmail.com>
2026-03-15 01:46:54 +0600

    showing the name ,population etc

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,42 +1,24 @@
-import React, { useState } from 'react';
-import './Country.css';
+import React from 'react';
 
-const Country = ({ country, handleVisitedCountries, handleVisitedFlag }) => {
-    const [visited, setVisited] = useState(false);
-    // console.log(country.area.area)
-
-    const handleVisited = () => {
-        // basic system
-        // if(visited){
-        //     setVisited(false)
-        // }
-        // else{
-        //     setVisited(true)
-        // }
-
-        // second system
-        // setVisited(visited ? false : true);
-
-        // third system
-        setVisited(!visited);
-        handleVisitedCountries(country);
-    }
-
+const Country = ({ country }) => {
+    console.log(country);
     return (
-        // <div className={`country border-lg text-center ${visited ? 'country-visited' : 'country-not-visited' }`}>
-        <div className={`country ${visited && 'country-visited'}`}>
-            <img src={country?.flags?.flags?.png} alt={country.flags.flags.alt} />
+        <div>
+            <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
             <h3>Name: {country.name.common}</h3>
             <p>Population: {country.population.population}</p>
-            <p>Area: {country.area.area} {
-                country.area.area > 300000 ? "Big Country" : 'Small country'
-            }</p>
-            <button onClick={handleVisited}>
-                {visited ? 'Visited' : 'Not Visited'}
-            </button>
-            <button onClick={() =>{ handleVisitedFlag(country?.flags?.flags?.png) }}>Add Visited Flag</button>
+            <p>Region: {country.region.region}</p>
+            <p>Capital: {country.capital.capital}</p>
+
+            {/* currencies is an object, so we need to use Object.values to get the values of the object and then access the symbol property of the first currency. If there are no currencies, we will display 'N/A'. */}
+
+            <p>Currencies: {Object.values(country.currencies?.currencies || country.currencies || {})[0]?.symbol || 'N/A'}</p>
+
+            <p>Languages: {Object.values(country.languages?.languages || country.languages || {}).join(', ')}</p>
+
         </div>
     );
+    
 };
 
 export default Country;
