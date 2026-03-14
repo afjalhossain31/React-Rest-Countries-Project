bc275882aab81687faa155dfd99eeafe20dec503
Afjal Hossain <abdullahalafjal31@gmail.com>
2026-03-15 02:03:55 +0600

    added button and handler

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,19 +1,22 @@
 import React, { use } from 'react';
 import Country from '../Country/Country';
+import './Countries.css';
 // countriesPromise ke distructure kore countriesData te rakha hocche
 
-const Countries = ({countriesPromise}) => {
+const Countries = ({ countriesPromise }) => {
     const countriesData = use(countriesPromise);
     const countries = countriesData.countries;
     console.log(countries);
     return (
         <div>
             <h1>In the Countries: {countries.length}</h1>
-            {
-                countries.map(country => <Country 
-                    key={country.cca3.cca3}
-                    country={country}></Country>)
-            }
+            <div className="countries">
+                {
+                    countries.map(country => <Country
+                        key={country.cca3}
+                        country={country}></Country>)
+                }
+            </div>
         </div>
     );
 };
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,24 +1,33 @@
 import React from 'react';
+import './Country.css';
 
 const Country = ({ country }) => {
     console.log(country);
+
+    const handleVisited = () => {
+        console.log('button clicked');
+    }
+
     return (
-        <div>
+        <div className='country'>
             <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
             <h3>Name: {country.name.common}</h3>
             <p>Population: {country.population.population}</p>
             <p>Region: {country.region.region}</p>
             <p>Capital: {country.capital.capital}</p>
-
             {/* currencies is an object, so we need to use Object.values to get the values of the object and then access the symbol property of the first currency. If there are no currencies, we will display 'N/A'. */}
 
-            <p>Currencies: {Object.values(country.currencies?.currencies || country.currencies || {})[0]?.symbol || 'N/A'}</p>
+            {/* <p>Currencies: {Object.values(country.currencies?.currencies || country.currencies || {})[0]?.symbol || 'N/A'}</p> */}
+            {/* 
+            <p>Languages: {Object.values(country.languages?.languages || country.languages || {}).join(', ')}</p> */}
+            <p>Area: {country.area.area} {country.area.area > 300000 ? " Big" : "Small country"}</p>
 
-            <p>Languages: {Object.values(country.languages?.languages || country.languages || {}).join(', ')}</p>
+            <button onClick={handleVisited}>visited</button>
+
 
         </div>
     );
-    
+
 };
 
 export default Country;
